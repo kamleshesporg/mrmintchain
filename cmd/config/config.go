62815e799c9a0d4c8ebde53781421f17d6419c65
kamleshesporg <kamlesh@espsofttech.org>
2025-05-14 17:57:34 +0530

    stake -> mnt

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -41,7 +41,7 @@ const (
 
 const (
 	// DisplayDenom defines the denomination displayed to users in client applications.
-	DisplayDenom = "mnt"
+	DisplayDenom = "photon"
 )
 
 // SetBech32Prefixes sets the global prefixes to be used when serializing addresses and public keys to Bech32 strings.
